a03008268a133a3703db957ae2c4782f11354597
kazuki-asai <alice.nine.in.wonder.land.0101@gmail.com>
2026-02-11 20:38:13 +0900

    rubocop

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,16 +2,16 @@ module UsersHelper
   ROLES = {
     "admin"=>"管理者",
     "owner"=>"店舗オーナー",
-    "staff"=>"店舗スタッフ",
+    "staff"=>"店舗スタッフ"
   }
 
   def roles_pulldown
     # 管理者の場合は全てのロール
     # owner の場合は、ownerとstaffのみ選択可能
     if current_user.admin?
-      User.roles.keys.map { |r| [ROLES[r], r] }
+      User.roles.keys.map { |r| [ ROLES[r], r ] }
     else
-      %w[owner staff].map { |r| [ROLES[r], r] }
+      %w[owner staff].map { |r| [ ROLES[r], r ] }
     end
   end
 
