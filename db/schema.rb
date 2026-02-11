cbffcfc8229d9701749857d7033ae20ac7bb9dca
kazuki-asai <alice.nine.in.wonder.land.0101@gmail.com>
2026-02-12 06:44:53 +0900

    画像管理用のテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_11_030535) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_11_130704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -47,6 +47,14 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_11_030535) do
     t.string "domain"
     t.string "name"
     t.datetime "updated_at", null: false
+  end
+
+  create_table "thumbnails", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.bigint "thumbnailable_id", null: false
+    t.string "thumbnailable_type", null: false
+    t.datetime "updated_at", null: false
+    t.index ["thumbnailable_type", "thumbnailable_id"], name: "index_thumbnails_on_thumbnailable"
   end
 
   create_table "users", force: :cascade do |t|
